4b676290bbba9476b862af1f464d58c8ea2e127d
MrDu <532933736@qq.com>
2021-11-29 15:55:07 +0800

    This is the second test!!

--- src/day01/Demo01.java
+++ src/day01/Demo01.java
@@ -3,5 +3,6 @@ package day01;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.print("This is the first test!");
+        System.out.print("This is the second test!");
     }
 }
